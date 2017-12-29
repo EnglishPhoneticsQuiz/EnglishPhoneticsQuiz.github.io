0248c220ccc862bc8d10cc567c6d389c077c7145
EnglishPhoneticsQuiz <34944527+EnglishPhoneticsQuiz@users.noreply.github.com>
2017-12-29 18:44:25 +0100

    Create EPJ1.js

---
+++ EPJ1.js
@@ -0,0 +1,161 @@
+// buttons functions
+
+function letterP() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = 'p'; } else {
+	document.getElementById("transcription").innerHTML += 'p';
+}};
+
+function letterB() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = 'b'; } else {
+	document.getElementById("transcription").innerHTML += 'b';
+}};
+
+function letterT() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = 't'; } else {
+	document.getElementById("transcription").innerHTML += 't';
+}};
+
+function letterD() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = 'd'; } else {
+	document.getElementById("transcription").innerHTML += 'd';
+}};
+function letterK() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = 'k'; } else {
+	document.getElementById("transcription").innerHTML += 'k';
+}};
+function letterG() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = 'g'; } else {
+	document.getElementById("transcription").innerHTML += 'g';
+}};
+function letterF() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = 'f'; } else {
+	document.getElementById("transcription").innerHTML += 'f';
+}};
+function letterV() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = 'v'; } else {
+	document.getElementById("transcription").innerHTML += 'v';
+}};
+function letterS() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = 's'; } else {
+	document.getElementById("transcription").innerHTML += 's';
+}};
+function letterH() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = 'h'; } else {
+	document.getElementById("transcription").innerHTML += 'h';
+}};
+function letterZ() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = 'z'; } else {
+	document.getElementById("transcription").innerHTML += 'z';
+}};
+function letterM() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = 'm'; } else {
+	document.getElementById("transcription").innerHTML += 'm';
+}};
+function letterN() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = 'n'; } else {
+	document.getElementById("transcription").innerHTML += 'n';
+}};
+function letterL() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = 'l'; } else {
+	document.getElementById("transcription").innerHTML += 'l';
+}};
+function letterR() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = 'r'; } else {
+	document.getElementById("transcription").innerHTML += 'r';
+}};
+function letterJ() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = 'j'; } else {
+	document.getElementById("transcription").innerHTML += 'j';
+}};
+function letterW() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = 'w'; } else {
+	document.getElementById("transcription").innerHTML += 'w';
+}};
+function letterth() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = '&#x3B8;'; } else {
+	document.getElementById("transcription").innerHTML += '&#x3B8;';
+}};
+function letterthd() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = '&#xF0;'; } else {
+	document.getElementById("transcription").innerHTML += '&#xF0;';
+}};
+function lettersh() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = '&#x283;'; } else {
+	document.getElementById("transcription").innerHTML += '&#x283;';
+}};
+function lettersgh() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = '&#x292;'; } else {
+	document.getElementById("transcription").innerHTML += '&#x292;';
+}};
+function lettergn() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = '&#x14B;'; } else {
+	document.getElementById("transcription").innerHTML += '&#x14B;';
+}};
+function letterAA() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = '&#x251;&#x2d0;'; } else {
+	document.getElementById("transcription").innerHTML += '&#x251;&#x2d0;';
+}};
+function letterEE() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = '&#x25c;&#x2d0;'; } else {
+	document.getElementById("transcription").innerHTML += '&#x25c;&#x2d0;';
+}};
+function letterii() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = 'i&#x2d0;'; } else {
+	document.getElementById("transcription").innerHTML += 'i&#x2d0;';
+}};
+function letteroo() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = '&#x254;&#x2d0;'; } else {
+	document.getElementById("transcription").innerHTML += '&#x254;&#x2d0;';
+}};
+function letteruu() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = 'u&#x2d0;'; } else {
+	document.getElementById("transcription").innerHTML += 'u&#x2d0;';
+}};
+function lettera() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = 'a'; } else {
+	document.getElementById("transcription").innerHTML += 'a';
+}};
+function letterAE() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = '&#xe6;'; } else {
+	document.getElementById("transcription").innerHTML += '&#xe6;';
+}};
+function letterA() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = '&#x28c;'; } else {
+	document.getElementById("transcription").innerHTML += '&#x28c;';
+}};
+function letterE() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = 'e'; } else {
+	document.getElementById("transcription").innerHTML += 'e';
+}};
+function letterschwa() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = '&#x259;'; } else {
+	document.getElementById("transcription").innerHTML += '&#x259;';
+}};
+function letterbitch() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = '&#x26A;'; } else {
+	document.getElementById("transcription").innerHTML += '&#x26A;';
+}};
+function letterI() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = 'i'; } else {
+	document.getElementById("transcription").innerHTML += 'i';
+}};
+function letterO() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = '&#x254;'; } else {
+	document.getElementById("transcription").innerHTML += '&#x254;';
+}};
+function letterOO() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = '&#x252;'; } else {
+	document.getElementById("transcription").innerHTML += '&#x252;';
+}};
+function lettershould() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = '&#x28a;'; } else {
+	document.getElementById("transcription").innerHTML += '&#x28a;';
+}};
+function letterU() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = 'u'; } else {
+	document.getElementById("transcription").innerHTML += 'u';
+}};
+function letterApostrofo() {
+	if ( document.getElementById("transcription").innerHTML == "&nbsp;") {document.getElementById("transcription").innerHTML = '&#x2c8;'; } else {
+	document.getElementById("transcription").innerHTML += '&#x2c8;';
+}};
