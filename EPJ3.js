e0d484120cf957263dfd626970622c9420fb4001
EnglishPhoneticsQuiz <34944527+EnglishPhoneticsQuiz@users.noreply.github.com>
2018-01-02 13:45:47 +0100

    Update EPJ3.js

--- EPJ3.js
+++ EPJ3.js
@@ -3,8 +3,12 @@
 //RANDOM PROGRAM
 var word = [];
 var trans = [];
+var prev = [];
+var prevtrans = [];
 var index = 0;
 var back = 0;
+var indexprev = 0;
+var counter = 0;
 var corr = 0;
 var wro = 0;
 
@@ -18,6 +22,15 @@ function nexttranscription() {
 		index = RndNumber();
 		document.getElementById("writeaword").innerHTML = word[index];
 
+
+//save the current word and transcription in the prev[]-prevtrans[] arrays
+		prev[indexprev] = word[index];
+		prevtrans[indexprev] = trans[index];
+		counter = indexprev;
+		indexprev++;
+		
+
+		
 //hide correct and wrong		
 		document.getElementById("correctorwrong").style.visibility = "hidden";
 	
@@ -28,6 +41,27 @@ function nexttranscription() {
 		document.getElementById("transcriptionshown").className = "normal";
 }
 
+
+//previous transcription
+function previoustranscription() {
+	if (counter == 0) {
+		alert("there aren't any previous words");
+	} else {
+	counter--;
+	document.getElementById("writeaword").innerHTML = prev[counter];
+	document.getElementById("transcriptionshown").innerHTML = prevtrans[counter];
+	document.getElementById("transcriptionshown").style.visibility = "visible";
+	document.getElementById("transcriptionshown").className = "wrong";
+	
+	}
+
+//hide correct and wrong		
+		document.getElementById("correctorwrong").style.visibility = "hidden";
+
+}
+
+
+
 //show transcription
 function showtranscription() {
 		document.getElementById("transcriptionshown").innerHTML = trans[index];
@@ -37,6 +71,57 @@ function showtranscription() {
 }
 
 	
+
+
+//clear all the transcriptions
+function cleartranscription() {
+	document.getElementById("transcription").innerHTML = '&nbsp;';
+	
+	document.getElementById("transcriptionshown").innerHTML = '&nbsp;';
+	document.getElementById("transcriptionshown").className = "normal";
+
+	document.getElementById("correctorwrong").style.visibility = "hidden";
+
+}
+
+//backspace
+function backspace() {
+	back = document.getElementById("transcription").innerHTML;
+	back = back.substring(0, back.length - 1);
+	//console.log(back.toString());
+	document.getElementById("transcription").innerHTML = back;
+	
+	if (document.getElementById("transcription").innerHTML == "") {
+		document.getElementById("transcription").innerHTML = '&nbsp;';
+	}
+}
+
+
+//check transcription
+function checktranscription() {
+	document.getElementById("transcriptionshown").innerHTML = trans[index];
+	document.getElementById("transcriptionshown").style.visibility = "hidden";
+	corr = document.getElementById("transcription").innerHTML;
+	wro = document.getElementById("transcriptionshown").innerHTML;
+	console.log(corr);
+	console.log(wro);
+	
+	if (corr == wro) {
+		document.getElementById("correctorwrong").innerHTML = "CORRECT!";
+		document.getElementById("correctorwrong").className = "correct";
+		document.getElementById("correctorwrong").style.visibility = "visible";
+		
+		
+	}
+	else {
+		document.getElementById("correctorwrong").innerHTML = "WRONG!";
+		document.getElementById("correctorwrong").className = "wrong";
+		document.getElementById("correctorwrong").style.visibility = "visible";
+	}
+}
+
+
+
 
 
 //clear all the transcriptions
